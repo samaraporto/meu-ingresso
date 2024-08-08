664fa56afaa624136206b7c7ee74f9c8138a7881
SAMARA PORTO DE NORONHA <20231ADSGR0260@labs.garanhuns.ifpe>
2024-08-07 21:28:00 -0300

    2

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,25 @@
 import {Router} from 'express'
 import { obterUsuarioPorEmail } from '../models/usuarios.js'
+import jwt from 'jsonwebtoken'
 
+
+const senha_secreta = "s3nh@sup3rsecreta"
 const routerUsuario = Router()
 //login
 //usuario: samaraporto@gmail.com
 //senha:  12345678
+routerUsuario.post('/api/token',(req, res)=>{
+    const {token} = req.body
+   try{
+    const isValid = jwt.verify(token, senha_secreta)
+    if(isValid){
+        return res.status(200).json({message: 'valid'})
+    }
+   } catch(e){
+       return res.status(404).json({message: 'invalid'})
 
+   }
+})
 routerUsuario.post('/api/login', async (req, res)=>{
     const {usuario, senha} = req.body
 
@@ -13,8 +27,9 @@ routerUsuario.post('/api/login', async (req, res)=>{
 
     if(u != usuario){
         //verificar a senha
-        if(u.senha == senha){
-            return res.status(200).json({taken:"000"})
+        if(u.senha === senha){
+            const token  = jwt.sign({usuario, regrasDeAcesso: "administrador"}, senha_secreta,{ expiresIn: "10s"})
+            return res.status(200).json({token})
 
         }else{
             return res.status(404).json({message: 'Usuário/senha incorreto'})
